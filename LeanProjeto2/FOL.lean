4083da4529c984569141a172877a4138a8c0664a
LiaMalato <lia.malato@hotmail.comm>
2024-06-26 12:04:49 +0100

    Versão Reunião

--- LeanProjeto2/FOL.lean
+++ LeanProjeto2/FOL.lean
@@ -1,4 +1,5 @@
 import LeanProjeto2
+import MathLib.Tactic
 
 namespace FOL
 
@@ -7,3 +8,11 @@ inductive FTipe : Type
 | ground : FTipe                 -- G
 | arrow : FTipe → FTipe → FTipe  -- σ → τ
 | star : FTipe → FTipe           -- σ*
+
+
+inductive P : Prop
+| x
+| y
+
+lemma a (x:P) : P := by
+  constructor
